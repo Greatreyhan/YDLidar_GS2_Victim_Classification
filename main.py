6cc74c571a0dce21c1588ddea3fe87f0854bc0bf
greatreyhan <greatreyhan@students.undip.ac.id>
2024-03-12 22:53:45 +0700

    Created model, not tested yet

---
+++ main.py
@@ -0,0 +1,48 @@
+import serial
+import matplotlib.pyplot as plt
+import cmath
+from YDLidarGS2 import YDLidar_GS2 as ydlidar
+
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='polar')
+ax.set_theta_direction(-1)
+ax.set_theta_zero_location('N')
+ax.set_title('lidar (exit: Key E)',fontsize=18)
+plt.connect('key_press_event', lambda event: exit(1) if event.key == 'e' else None)
+
+
+
+ydl = ydlidar(port='/dev/ttyUSB0')
+
+ydl.startlidar()
+
+def log_data(file_path, data):
+    # Fungsi ini menyimpan data ke dalam file
+    with open(file_path, 'a') as file:
+        file.write(','.join(map(str, data)) + '\n')
+
+counter = 0
+while counter < 2000:
+    rt = ydl.getData()
+    ltheta = rt.thetas[:80]
+    rtheta = rt.thetas[80:]
+    log_data("./log_left.txt",rt.distance)
+    
+    if('line' in locals()):
+        line.remove()
+    if('line2' in locals()):
+        line2.remove()
+    line = ax.scatter(ltheta, rt.distance[:80], c="red", s=5)
+    line2 = ax.scatter(rtheta, rt.distance[80:], c="blue", s=5)
+    ax.set_theta_offset(cmath.pi / 2)
+    ax.set_theta_offset(cmath.pi / 2)
+    plt.pause(0.00001)
+    counter+=1
+    
+
+
+ydl.ser.write([0xA5,0xA5,0xA5,0xA5,0x00,0x64,0x00,0x00,0x64])
+ydl.ser.close()
+
+
